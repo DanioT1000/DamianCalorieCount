7dcff480aa4f052d34acb843db5a6de00d1874f6
DanioT1000 <danio1serek@gmail.com>
2015-02-20 12:05:15 +0100

    I checked id database work correctly

--- src/com/example/damiancaloriecount/SearchScreen.java
+++ src/com/example/damiancaloriecount/SearchScreen.java
@@ -2,6 +2,7 @@ package com.example.damiancaloriecount;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SearchScreen extends Activity {
 
@@ -11,6 +12,15 @@ public class SearchScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_screen);
+		
+		tvSearchResults = (TextView)findViewById(R.id.textView1);
+		
+		CRUDdb info = new CRUDdb(this);
+		info.open();
+		String data = info.getData();
+		info.close();
+		tvSearchResults.setText(data);
 	}
 
 }
