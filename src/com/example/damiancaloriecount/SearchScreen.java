10456addb6cf85cab5e916ce3e57da3583e5f652
DanioT1000 <danio1serek@gmail.com>
2015-02-20 12:04:15 +0100

    I checked if database work correct

--- src/com/example/damiancaloriecount/SearchScreen.java
+++ src/com/example/damiancaloriecount/SearchScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class SearchScreen extends Activity {
 
+	TextView tvSearchResults;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
