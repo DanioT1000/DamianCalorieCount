bb24a888605496718bde54604861ff9866b1499d
DanioT1000 <danio1serek@gmail.com>
2015-02-23 21:18:34 +0100

    Added two methods: addToDiary and getAllProductToday

--- src/com/example/damiancaloriecount/CRUDdb.java
+++ src/com/example/damiancaloriecount/CRUDdb.java
@@ -1,5 +1,8 @@
 package com.example.damiancaloriecount;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import android.content.ContentValues;
@@ -115,6 +118,50 @@ public class CRUDdb {
 			info.add(c.getString(iProduct_Protein));
 			info.add(c.getString(iProduct_Fat));
 			info.add(c.getString(iProduct_Kcal));
+		}
+		
+		return info;
+	}
+	
+	public long addToDiary(String product_name, float carbs, float protein, float fat, float kcal){
+		Calendar c = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ");
+		String strDate = sdf.format(c.getTime());
+		
+		ContentValues cv = new ContentValues();
+		cv.put(DBHelper.COLUMN_DIARY_DATE, strDate);
+		cv.put(DBHelper.COLUMN_DIARY_PRODUCT_NAME, product_name);
+		cv.put(DBHelper.COLUMN_DIARY_CARBS, carbs);
+		cv.put(DBHelper.COLUMN_DIARY_PROTEIN, protein);
+		cv.put(DBHelper.COLUMN_DIARY_FAT , fat);
+		cv.put(DBHelper.COLUMN_DIARY_KCAL, kcal);
+		return ourDatabase.insert(DBHelper.TABLE_DIARY, null, cv);	
+	}
+	
+	public ArrayList<Product> getAllProductsToday(){
+		String[] columns = new String[]{DBHelper.COLUMN_DIARY_ID , DBHelper.COLUMN_DIARY_DATE, DBHelper.COLUMN_DIARY_PRODUCT_NAME, DBHelper.COLUMN_DIARY_CARBS, DBHelper.COLUMN_DIARY_PROTEIN, DBHelper.COLUMN_DIARY_FAT, DBHelper.COLUMN_DIARY_KCAL};
+		Cursor c = ourDatabase.query(DBHelper.TABLE_DIARY, columns, null, null, null, null, null);
+		
+		ArrayList<Product> info = new ArrayList<Product>();
+		
+		int iProduct_ID = c.getColumnIndex(DBHelper.COLUMN_DIARY_ID);
+		int iProduct_Date = c.getColumnIndex(DBHelper.COLUMN_DIARY_DATE);
+		int iProduct_Name = c.getColumnIndex(DBHelper.COLUMN_DIARY_PRODUCT_NAME);
+		int iProduct_Carbs = c.getColumnIndex(DBHelper.COLUMN_DIARY_CARBS);
+		int iProduct_Protein = c.getColumnIndex(DBHelper.COLUMN_DIARY_PROTEIN);
+		int iProduct_Fat = c.getColumnIndex(DBHelper.COLUMN_DIARY_FAT);
+		int iProduct_Kcal = c.getColumnIndex(DBHelper.COLUMN_DIARY_KCAL);
+		
+		// make this better, loop only one time
+		for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext()){
+			Product product = new Product();
+			product.setName(c.getString(iProduct_Name));
+			product.setCarbs(Float.parseFloat(c.getString(iProduct_Carbs)));
+			product.setProtein(Float.parseFloat(c.getString(iProduct_Protein)));
+			product.setFat(Float.parseFloat(c.getString(iProduct_Fat)));
+			product.setKcal(Float.parseFloat(c.getString(iProduct_Kcal)));
+			
+			info.add(product);
 		}
 		
 		return info;
