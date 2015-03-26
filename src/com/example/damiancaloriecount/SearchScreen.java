aed052e510b0c46c70a4274b9a4dd07df6e49571
DanioT1000 <danio1serek@gmail.com>
2015-03-26 15:25:38 +0100

    Fixed search interface Added asyncTask

--- src/com/example/damiancaloriecount/SearchScreen.java
+++ src/com/example/damiancaloriecount/SearchScreen.java
@@ -2,62 +2,48 @@ package com.example.damiancaloriecount;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.internal.widget.AdapterViewCompat;
 import android.support.v7.internal.widget.AdapterViewCompat.OnItemClickListener;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SearchScreen extends Activity implements OnClickListener {
+public class SearchScreen extends Activity {
 
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-	}
-
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-	}
-
-	//String[] name = {"cos", "cosik", "cosik2", "cosik3", "agata", "barbara", "dupa"};
-	AutoCompleteTextView autoTextView;
-	Button bSearch;
+	EditText etSearch;
 	ListView listView;
 	CRUDdb info;
-	ArrayAdapter<String> adapter2;
+	ArrayAdapter<String> adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_screen);
-
-		autoTextView = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
-		bSearch = (Button)findViewById(R.id.bSearch);
+		setContentView(R.layout.search_screen);
+		
+		//xml elements
+		etSearch = (EditText) findViewById(R.id.etSearch);
 		listView = (ListView)findViewById(R.id.listView);
 		
+		//database
 		info = new CRUDdb(this);
 		info.open();
 		
-		//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, name);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,info.getDataArrayList() );
-		
-		
-		autoTextView.setThreshold(1);
-		autoTextView.setAdapter(adapter);
-		
-		
-		bSearch.setOnClickListener(this);
+		//set names to adapter in asynctask
+		new DownloadProductNames().execute();
+			
+		//actions
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			@Override
@@ -66,27 +52,37 @@ public class SearchScreen extends Activity implements OnClickListener {
 				Object o = parent.getItemAtPosition(position);
 	            String keyword = o.toString();
 				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), "Parent: "+parent+"" + "postion: "+position+" id: "+ id+" text: "+ keyword, 0).show();
 				Intent i = new Intent(SearchScreen.this, AddToDiaryScreen.class);
 				i.putExtra("p_name", keyword);
 				startActivity(i);
 			}
 		});
 		
+		etSearch.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				//filter
+				String text = etSearch.getText().toString();
+				adapter.getFilter().filter(text);
+				
+			}
+		});
 		
-
-	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub 
-		switch(v.getId()){
-		case R.id.bSearch:
-			String productName = autoTextView.getText().toString();
-			ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,info.searchDataAndReturnArrayList(productName) );
-			listView.setAdapter(adapter2);
-			break;
-		}
 	}
 	
 	  @Override
@@ -100,5 +96,24 @@ public class SearchScreen extends Activity implements OnClickListener {
 	        super.onPause();
 	        info.close();
 	    }
+	    
+	    
+	private class DownloadProductNames extends AsyncTask<Void, Void, Void>{
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			
+			//data form db
+			adapter = new ArrayAdapter<String>(SearchScreen.this, android.R.layout.simple_dropdown_item_1line,info.getDataArrayList() );
+			listView.setAdapter(adapter);
+			
+			return null;
+		}
+		
+	} //end class
 
 }
+
+
+
